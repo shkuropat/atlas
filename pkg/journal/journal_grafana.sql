6e57ac2f1734ea67a7e2876036eb4c7a79347c6d
Vladislav Klimenko <sunsingerus@gmail.com>
2020-08-10 18:23:29 +0300

    Grafana Dashoard

--- pkg/journal/journal_grafana.sql
+++ pkg/journal/journal_grafana.sql
@@ -40,6 +40,28 @@ WHERE $timeFilter
 GROUP BY t
 ORDER BY t
 
+-- Bootguard endpoint calls count
+SELECT
+  $timeSeries as t,
+  count()
+FROM $table
+WHERE $timeFilter
+  AND endpoint_id=10
+  AND action_id=1
+GROUP BY t
+ORDER BY t
+
+-- Bootguard endpoint errors count
+SELECT
+  $timeSeries as t,
+  count()
+FROM $table
+WHERE $timeFilter
+  AND endpoint_id=10
+  AND action_id=7
+GROUP BY t
+ORDER BY t
+
 -- Reports endpoint calls count
 SELECT
   $timeSeries as t,
@@ -90,3 +112,20 @@ WHERE $timeFilter
   AND endpoint_id=2
 GROUP BY t, call_id
 ORDER BY t
+
+SELECT
+  $timeSeries as t,
+  JSONExtractBool(data, 'BootguardInfoParsed') AS BootguardInfoParsed,
+  JSONExtractBool(data, 'KMFound') AS KMFound,
+  JSONExtractBool(data, 'KMSignatureVerificationOk') AS KMSignatureVerificationOk,
+  JSONExtractBool(data, 'BPMFound') AS BPMFound,
+  JSONExtractBool(data, 'BPMKeyHashEqKMKeyHash') AS BPMKeyHashEqKMKeyHash,
+  JSONExtractBool(data, 'BPMSignatureVerificationOk') AS BPMSignatureVerificationOk,
+  JSONExtractBool(data, 'BGProtectedRangesFound') AS BGProtectedRangesFound,
+  JSONExtractBool(data, 'BGProtectedRangesHashVerificationOk') AS BGProtectedRangesHashVerificationOk,
+  JSONExtractBool(data, 'VendorProtectedRangesFound') AS VendorProtectedRangesFound,
+  JSONExtractBool(data, 'VendorProtectedRangesHashVerificationOk') AS VendorProtectedRangesHashVerificationOk,
+  JSONExtractRaw(data, 'BGErrors') AS BGErrors
+FROM $table
+WHERE $timeFilter
+AND LENGTH(data) > 0
