8f9682f901cead5054420d95febf3daa59693de7
Vladislav Klimenko <sunsingerus@gmail.com>
2021-07-18 14:30:57 +0300

    files walker

--- pkg/adapter/task_minio.go
+++ pkg/adapter/task_minio.go
@@ -18,6 +18,7 @@ import (
 	"github.com/binarly-io/atlas/pkg/api/atlas"
 	"github.com/binarly-io/atlas/pkg/config/sections"
 	"github.com/binarly-io/atlas/pkg/minio"
+	minio_go "github.com/minio/minio-go/v6"
 )
 
 // TaskMinIO
@@ -34,57 +35,150 @@ func NewTaskMinIOAdapter(config sections.MinIOConfigurator, task *atlas.Task) *T
 	}
 }
 
+const (
+	task   = "task"
+	in     = "in"
+	out    = "out"
+	result = "result"
+)
+
 // GetBucket
 func (a *TaskMinIO) GetBucket() string {
 	return a.Config.GetMinIOBucket()
 }
 
+// getPath
+func (a *TaskMinIO) getPath(what string) string {
+	switch what {
+	case task:
+		return a.Task.GetUUID().String()
+	case in:
+		return minio.PathJoin(a.GetTaskPath(), "in")
+	case out:
+		return minio.PathJoin(a.GetTaskPath(), "out")
+	case result:
+		return minio.PathJoin(a.GetTaskPath(), "result")
+	}
+	return ""
+}
+
+// getPrefix
+func (a *TaskMinIO) getPrefix(what string) string {
+	switch what {
+	case task:
+		return a.GetTaskPath() + "/"
+	case in:
+		return a.GetInPath() + "/"
+	case out:
+		return a.GetOutPath() + "/"
+	case result:
+		return a.GetResultPath() + "/"
+	}
+	return ""
+}
+
+// getFile
+func (a *TaskMinIO) getFile(what, file string) string {
+	switch what {
+	case task:
+		return minio.PathJoin(a.GetTaskPath(), file)
+	case in:
+		return minio.PathJoin(a.GetInPath(), file)
+	case out:
+		return minio.PathJoin(a.GetOutPath(), file)
+	case result:
+		return minio.PathJoin(a.GetResultPath(), file)
+	}
+	return ""
+}
+
+// walkFiles
+func (a *TaskMinIO) walkFiles(what string, f func(*minio.MinIO, *minio_go.ObjectInfo, *atlas.S3Address) bool) error {
+	mi, err := minio.NewMinIOFromConfig(a.Config)
+	if err != nil {
+		return err
+	}
+	bucket := a.GetBucket()
+	list, err := mi.List(bucket, a.getPrefix(what), -1)
+	if err != nil {
+		return err
+	}
+
+	for i := range list {
+		info := list[i]
+		s3address := atlas.NewS3Address(bucket, info.Key)
+		if !f(mi, &info, s3address) {
+			break
+		}
+	}
+	return nil
+}
+
+/***********************/
+/*     Wrappers        */
+/***********************/
+
 // GetTaskPath
 func (a *TaskMinIO) GetTaskPath() string {
-	return a.Task.GetUUID().String()
+	return a.getPath(task)
 }
 
 // GetInPath
 func (a *TaskMinIO) GetInPath() string {
-	return minio.PathJoin(a.GetTaskPath(), "in")
+	return a.getPath(in)
 }
 
 // GetInPrefix
 func (a *TaskMinIO) GetInPrefix() string {
-	return a.GetInPath() + "/"
+	return a.getPrefix(in)
 }
 
 // GetInFile
 func (a *TaskMinIO) GetInFile(file string) string {
-	return minio.PathJoin(a.GetInPath(), file)
+	return a.getFile(in, file)
+}
+
+// WalkInFiles
+func (a *TaskMinIO) WalkInFiles(f func(*minio.MinIO, *minio_go.ObjectInfo, *atlas.S3Address) bool) error {
+	return a.walkFiles(in, f)
 }
 
 // GetOutPath
 func (a *TaskMinIO) GetOutPath() string {
-	return minio.PathJoin(a.GetTaskPath(), "out")
+	return a.getPath(out)
 }
 
 // GetOutPrefix
 func (a *TaskMinIO) GetOutPrefix() string {
-	return a.GetOutPath() + "/"
+	return a.getPrefix(out)
 }
 
 // GetOutFile
 func (a *TaskMinIO) GetOutFile(file string) string {
-	return minio.PathJoin(a.GetOutPath(), file)
+	return a.getFile(out, file)
+}
+
+// WalkOutFiles
+func (a *TaskMinIO) WalkOutFiles(f func(*minio.MinIO, *minio_go.ObjectInfo, *atlas.S3Address) bool) error {
+	return a.walkFiles(out, f)
 }
 
 // GetResultPath
 func (a *TaskMinIO) GetResultPath() string {
-	return minio.PathJoin(a.GetTaskPath(), "result")
+	return a.getPath(result)
 }
 
 // GetResultPrefix
 func (a *TaskMinIO) GetResultPrefix() string {
-	return a.GetResultPath() + "/"
+	return a.getPrefix(result)
 }
 
 // GetResultFile
 func (a *TaskMinIO) GetResultFile(file string) string {
-	return minio.PathJoin(a.GetResultPath(), file)
+	return a.getFile(result, file)
+}
+
+// WalkResultFiles
+func (a *TaskMinIO) WalkResultFiles(f func(*minio.MinIO, *minio_go.ObjectInfo, *atlas.S3Address) bool) error {
+	return a.walkFiles(result, f)
 }
